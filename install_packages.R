3c9c0a8e6ec4221c3b1777322994939c31f6b9ad
bdwilliamson <brianw26@uw.edu>
2020-04-24 08:17:04 -0700

    add mice, stabs, knockoff, boot, vimp

--- install_packages.R
+++ install_packages.R
@@ -1,9 +1,16 @@
 # install r packages
 
-tidy_packages <- c("tidyverse", "ggplot2")
-dev_packages <- c("devtools", "remotes")
-regression_packages <- c("survival", "sandwich", "glmnet", "xgboost", "ranger", "SuperLearner", "geeM", "lme4")
+tidy_packages <- c("tidyverse", "ggplot2", "cowplot")
+dev_packages <- c("devtools", "remotes", "argparse")
+regression_packages <- c("survival", "sandwich", "glmnet", "xgboost", "ranger", "SuperLearner", "geeM", "lme4", "boot", "vimp")
+missing_data_packages <- c("mice")
+feature_selection_packages <- c("knockoffs", "stabs")
 bayes_packages <- c("R2OpenBUGS", "rstan")
+markdown_packages <- c("kableExtra", "bookdown")
 misc_packages <- c("xtable")
 
-install.packages(c(tidy_packages, dev_packages, regression_packages, bayes_packages, misc_packages), dep = TRUE, repos = "https://cran.rstudio.com/")
+install.packages(c(tidy_packages, dev_packages, 
+                   regression_packages, missing_data_packages,
+                   feature_selection_packages, bayes_packages, 
+                   misc_packages), dep = TRUE, 
+                 repos = "https://cran.rstudio.com/")
