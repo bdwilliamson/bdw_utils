0ed7892171556df53b8349765afb6f5f1413c810
Brian Williamson <bwillia2@fredhutch.org>
2021-11-19 14:46:33 -0800

    Add here, data.table to package list

--- install_packages.R
+++ install_packages.R
@@ -1,6 +1,6 @@
 # install r packages
 
-tidy_packages <- c("tidyverse", "ggplot2", "cowplot")
+tidy_packages <- c("tidyverse", "ggplot2", "cowplot", "here", "data.table")
 dev_packages <- c("devtools", "remotes", "argparse")
 regression_packages <- c("survival", "sandwich", "glmnet", "xgboost", "ranger", "SuperLearner", "geeM", "lme4", "boot", "vimp")
 missing_data_packages <- c("mice")
